9fc0273f48ff3e11246451173292848c3fb51ac7
Your Name <samara1381@mail.ru>
2022-04-30 11:23:40 +0300

    Объекты событий 2

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,14 +1,24 @@
 const activeUl = document.querySelector("ul");
+const mainDiv = document.querySelector(".dropdown");
+const itemsArray = Array.from(document.getElementsByClassName('dropdown__item'));
+const itemValue = document.querySelector('.dropdown__value');
 
 function ulClick() {
 	if (activeUl.classList.contains("dropdown__list_active") === true) {
 		activeUl.classList.remove("dropdown__list_active");
-		} else {
+	} else {
 			activeUl.classList.add("dropdown__list_active");
 		}
 }
 
-activeUl.addEventListener("click", ulClick);
+function itemClick(a) {
+    a.preventDefault()
+    ulClick();
+    itemValue.textContent = this.textContent;
+};
 
-//let paragraph = document.querySelector('p');
-//console.log(paragraph.textContent);
+for (let item of itemsArray) {
+    item.addEventListener('click', itemClick);
+};
+
+mainDiv.addEventListener("click", ulClick);
--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -7,10 +7,18 @@ function tabClick() {
 		let currentTab = tabsArray[i];
 		if (currentTab.classList.contains('tab_active')) {
 				currentTab.classList.remove('tab_active');
-          
+				tabsContentArray[tabsArray.indexOf(currentTab)].classList.remove('tab__content_active');
 		}
-    }; 
+    }
+	this.classList.add('tab_active');
+    tabsContentArray[tabsArray.indexOf(this)].classList.add('tab__content_active');
+	
+/* 	console.log(this);*/
 }
 
-document.addEventListener('click', tabClick);
+tabsArray.forEach(element => {
+    element.addEventListener('click', tabClick);
+})
+
+
 
