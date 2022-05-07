3f23515c85658b7af384e704b7b2a3c7e4c33b49
Your Name <samara1381@mail.ru>
2022-05-07 18:52:48 +0300

    DOM 2

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,10 +1,20 @@
-
 const arrRotator = Array.from(document.getElementsByClassName('rotator__case'));
 
 function adsRotator() {
-    arrRotator.forEach((e) => {
-        e.classList.toggle('rotator__case_active');       
-    });
-};
+	let index = 0;
+	setInterval(() => {
+		arrRotator.forEach((e) => {
+			if (e.classList.contains('rotator__case_active')) {
+					e.classList.remove('rotator__case_active');
+			};
+		});
+		if (index < arrRotator.length - 1) {
+			index++
+		} else {
+			index = 0;
+		}
+		arrRotator[index].classList.add('rotator__case_active');
+	}, 1000);
+}
 
-setInterval(adsRotator, 1000);
+adsRotator();
--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,19 +1,25 @@
-
+ 
 const revealArray = Array.from(document.getElementsByClassName('reveal'));
-
-function scroller() {
-	for(let i = 0; i++; i < revealArray.length - 1) {
-		elementTop = revealArray[i].getBoundingClientRect().top;
-		elementBottom = revealArray[i].getBoundingClientRect().bottom;
-		if ((elementTop < window.innerHeight) && (elementBottom > window.innerHeight)) {
-			revealArray[i].classList.add('reveal_active');
-		}
-	}
-} 
 
 window.addEventListener('scroll', scroller);
 
+function scroller() {
+    revealArray.forEach((e) => {
+        if (e.getBoundingClientRect().y < window.innerHeight && e.getBoundingClientRect().y > 0) {
+            e.classList.add('reveal_active');
+        } else {
+			e.classList.remove('reveal_active');
+		}
+    })
+}
 
-
-
-
+/* function scroller() {
+	for (let i = 0; i++; i < revealArray.length - 1) {
+		let elementTop = revealArray[i].getBoundingClientRect().top;
+		let elementBottom = revealArray[i].getBoundingClientRect().bottom;
+		if ((elementTop < window.innerHeight) && (elementBottom > window.innerHeight)) {
+			revealArray[i].classList.add('reveal_active');
+		}  
+	}
+} 
+ */ 
