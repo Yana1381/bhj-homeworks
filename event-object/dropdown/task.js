6038d71eb4b658cd95d47e9eb19a3ab3fd57ee5a
Your Name <samara1381@mail.ru>
2022-05-09 11:39:47 +0300

    Объекты событий 3

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -4,11 +4,7 @@ const itemsArray = Array.from(document.getElementsByClassName('dropdown__item'))
 const itemValue = document.querySelector('.dropdown__value');
 
 function ulClick() {
-	if (activeUl.classList.contains("dropdown__list_active") === true) {
-		activeUl.classList.remove("dropdown__list_active");
-	} else {
-			activeUl.classList.add("dropdown__list_active");
-		}
+	activeUl.classList.toggle("dropdown__list_active");
 }
 
 function itemClick(a) {
@@ -17,8 +13,11 @@ function itemClick(a) {
     itemValue.textContent = this.textContent;
 };
 
+itemValue.addEventListener("click", ulClick);
+
 for (let item of itemsArray) {
     item.addEventListener('click', itemClick);
 };
 
-mainDiv.addEventListener("click", ulClick);
+
+
